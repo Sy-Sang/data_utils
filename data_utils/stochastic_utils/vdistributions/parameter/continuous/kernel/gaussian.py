61b4d47cce44f1a6a7fbc8bfe1047dc45cb435bb
martin9ale <sangsiyuan@me.com>
2025-05-27 18:07:32 +0800

    2025/05/27 周二 18:07:32.32, from office

--- data_utils/stochastic_utils/vdistributions/parameter/continuous/kernel/gaussian.py
+++ data_utils/stochastic_utils/vdistributions/parameter/continuous/kernel/gaussian.py
@@ -24,23 +24,29 @@ from collections import namedtuple
 from data_utils.stochastic_utils.vdistributions.abstract import AbstractDistribution, eps
 from data_utils.stochastic_utils.vdistributions.parameter.continuous.basic import NormalDistribution
 from data_utils.stochastic_utils.vdistributions.parameter.abstract import ParameterDistribution
+from data_utils.stochastic_utils.vdistributions.tools.divergence import kl_divergence_continuous, \
+    js_divergence_continuous
 
 # 外部模块
 import numpy
 from scipy.stats import t, iqr
 from scipy.interpolate import interp1d
-import numpy
+from scipy.optimize import minimize
 
 
 # 代码块
 
-class GaussianKernelMixDistribution(AbstractDistribution):
+class KernelMixDistribution(AbstractDistribution):
+    """核混合分布"""
+
+
+class GaussianKernelMixDistribution(KernelMixDistribution):
 
     def __init__(self, *args):
         super().__init__()
         self.args = args
         self.kernels = [
-            NormalDistribution(i[0], i[1]) for i in args
+            NormalDistribution(i[0], numpy.clip(i[1], eps, numpy.inf)) for i in args
         ]
 
         self.domain_min = self.kernels[0].domain().min
@@ -85,7 +91,7 @@ class GaussianKernelMixDistribution(AbstractDistribution):
         return r if r.shape != (1,) else r[0]
 
 
-class GaussianKernelWeightedMixDistribution(AbstractDistribution):
+class WeightedGaussianKernelMixDistribution(KernelMixDistribution):
 
     def __init__(self, *args):
         super().__init__()
@@ -95,7 +101,7 @@ class GaussianKernelWeightedMixDistribution(AbstractDistribution):
 
         for i in args:
             mu, sigma, weight = i
-            self.kernels.append(NormalDistribution(mu, sigma))
+            self.kernels.append(NormalDistribution(mu, numpy.clip(sigma, eps, numpy.inf)))
             self.weights.append(weight)
 
         self.weights = numpy.asarray(self.weights, dtype=float)
@@ -112,17 +118,27 @@ class GaussianKernelWeightedMixDistribution(AbstractDistribution):
         self.ppf_inter = interp1d(cdf_vals, x_grid, bounds_error=False, fill_value=(x_grid[0], x_grid[-1]))
 
     def __str__(self):
-        return str({self.__class__.__name__: self.kernels})
+        return str({self.__class__.__name__: self.kernels, "weight": self.weights})
 
     def __repr__(self):
-        return str({self.__class__.__name__: self.kernels})
+        return str({self.__class__.__name__: self.kernels, "weight": self.weights})
 
-    def kernel_data(self, sort_index=0):
+    def kernel_weight_data(self, sort_index=0):
         d = []
         for k, w in zip(self.kernels, self.weights):
             d.append([k.mu, k.sigma, w])
         d = numpy.asarray(d)
         return d[numpy.argsort(d[:, sort_index])]
+
+    def kernel_data(self, sort_index=0):
+        d = []
+        for k in self.kernels:
+            d.append([k.mu, k.sigma])
+        d = numpy.asarray(d)
+        if sort_index is None:
+            return d
+        else:
+            return d[numpy.argsort(d[:, sort_index])]
 
     def pdf(self, x, *args, **kwargs):
         x = numpy.atleast_1d(x)
@@ -142,10 +158,118 @@ class GaussianKernelWeightedMixDistribution(AbstractDistribution):
         cliped_result = numpy.clip(result, self.domain_min, self.domain_max)
         return cliped_result if cliped_result.shape != (1,) else cliped_result[0]
 
-def divergenced_gaussian_kernel_mix_distribution(dist: GaussianKernelMixDistribution, kl_divergence_value:float):
+
+def divergenced_gaussian_kernel_mix_distribution(
+        dist: KernelMixDistribution,
+        kl_divergence_value: float,
+        kernel_num: int = None
+) -> GaussianKernelMixDistribution:
+    """kl散度约束的混合核分布"""
+
+    def target_function(x):
+        x = numpy.asarray(x).reshape(-1, 2)
+        new_dist = GaussianKernelMixDistribution(*x)
+        kl = js_divergence_continuous(dist, new_dist)
+        return (kl - numpy.clip(kl_divergence_value, 0, 0.6931)) ** 2
+
+    kernels = dist.kernel_data()
+    # kernels = numpy.column_stack((kernels, [1] * len(kernels)))
+    if kernel_num is None:
+        additional_kernel_num = 0
+    else:
+        additional_kernel_num = int(numpy.clip(kernel_num, 1, numpy.inf)) - len(kernels)
+
+    if additional_kernel_num == 0:
+        additional_kernel = numpy.asarray([])
+        method = "trust-constr"
+    elif additional_kernel_num > 0:
+        additional_kernel = numpy.asarray([0, 1] * additional_kernel_num)
+        method = "L-BFGS-B"
+    else:
+        additional_kernel = numpy.asarray([])
+        kernels = kernels[:int(numpy.clip(kernel_num, 1, numpy.inf))]
+        method = "L-BFGS-B"
+
+    bound_list = []
+    for _ in range(kernel_num):
+        bound_list.append((None, None))
+        bound_list.append((0.1, None))
+        # bound_list.append((0, 1))
+
+    optimize_kernels = minimize(
+        target_function,
+        numpy.concatenate((kernels.reshape(-1), additional_kernel)),
+        bounds=bound_list,
+        method=method,
+        options={'maxiter': 100, 'disp': False}
+    )
+    optimize_kernels_data = numpy.asarray(optimize_kernels.x).reshape(-1, 2)
+    return GaussianKernelMixDistribution(*optimize_kernels_data)
+
+
+def divergenced_weight_kernel_mix_distribution(
+        dist: KernelMixDistribution,
+        kl_divergence_value: float,
+        kernel_num: int = None
+) -> WeightedGaussianKernelMixDistribution:
+    """kl散度约束的混合核分布"""
+
+    def target_function(x):
+        x = numpy.asarray(x).reshape(-1, 3)
+        new_dist = WeightedGaussianKernelMixDistribution(*x)
+        kl = js_divergence_continuous(dist, new_dist)
+        return (kl - numpy.clip(kl_divergence_value, 0, 0.6931)) ** 2
+
+    kernels = dist.kernel_data()
+    kernels = numpy.column_stack((kernels, numpy.random.uniform(0, 1, len(kernels))))
+
+    if kernel_num is None:
+        additional_kernel_num = 0
+    else:
+        additional_kernel_num = int(numpy.clip(kernel_num, 1, numpy.inf)) - len(kernels)
+
+    if additional_kernel_num == 0:
+        additional_kernel = numpy.asarray([])
+        method = "trust-constr"
+    elif additional_kernel_num > 0:
+        additional_kernel = numpy.asarray([numpy.random.uniform(-1, 1), 1, 1] * additional_kernel_num)
+        method = "L-BFGS-B"
+    else:
+        additional_kernel = numpy.asarray([])
+        kernels = kernels[:int(numpy.clip(kernel_num, 1, numpy.inf))]
+        method = "L-BFGS-B"
+
+    bound_list = []
+    for _ in range(kernel_num):
+        bound_list.append((None, None))
+        bound_list.append((0.1, None))
+        bound_list.append((0, 1))
+
+    optimize_kernels = minimize(
+        target_function,
+        numpy.concatenate((kernels.reshape(-1), additional_kernel)),
+        bounds=bound_list,
+        method=method,
+        options={'maxiter': 100, 'disp': False}
+    )
+    optimize_kernels_data = numpy.asarray(optimize_kernels.x).reshape(-1, 3)
+    return WeightedGaussianKernelMixDistribution(*optimize_kernels_data)
 
 
 if __name__ == "__main__":
-    gkmd = GaussianKernelWeightedMixDistribution((0, 0.1, 1), (0, 0.1, 1))
-    # print(gkmd.rvf(100))
-    print(gkmd.ppf(0.1))
+    from matplotlib import pyplot as plt
+
+    # gkmd = GaussianKernelWeightedMixDistribution((0, 0.1, 1), (0, 0.1, 1))
+    # # print(gkmd.rvf(100))
+    # print(gkmd.ppf(0.1))
+    dist = WeightedGaussianKernelMixDistribution([0, 1, 1], [1, 2, 1], [2, 1, 1])
+    new_dist = divergenced_weight_kernel_mix_distribution(dist, 0.01, 4)
+    print(
+        js_divergence_continuous(dist, new_dist)
+    )
+    print(new_dist)
+    _, pdf, _ = dist.curves(1000)
+    plt.plot(pdf[:, 0], pdf[:, 1])
+    _, pdf, _ = new_dist.curves(1000)
+    plt.plot(pdf[:, 0], pdf[:, 1])
+    plt.show()
